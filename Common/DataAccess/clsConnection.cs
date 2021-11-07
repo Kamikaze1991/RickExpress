a602474f838e02ee932efb7db247206ff07247e3
Richi BPinargote <richyprg@gmail.com>
2021-11-06 22:21:58 -0500

    inicio correcto

---
+++ Common/DataAccess/clsConnection.cs
@@ -0,0 +1,63 @@
+﻿using Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.DataAccess
+{
+    public class clsConnection
+    {
+        const string strConnection = "Data Source=192.168.1.1,username=sa,password=Aezakami123";
+        SqlConnection mSqlConnection;
+        public DataSet objResult { get; set; }
+
+        /// <summary>
+        /// typical construc
+        /// </summary>
+        public clsConnection() {
+            mSqlConnection = new SqlConnection(strConnection);
+        }
+
+        /// <summary>
+        /// method for execute any querys and response a dataset
+        /// </summary>
+        /// <param name="strCommand"></param>
+        /// <param name="lstParameters"></param>
+        /// <returns>return a dataset value</returns>
+        bool executeCommand(string strCommand, clsSqlParameter [] lstParameters)
+        {
+            try {
+                SqlCommand mCommand=new SqlCommand(strCommand, mSqlConnection);
+                SqlParameter[] defparameters=new SqlParameter[lstParameters.Length];
+
+                for (int i = 0; i < lstParameters.Length; i++) 
+                {
+                    defparameters[i] = new SqlParameter();
+                    defparameters[i].ParameterName = lstParameters[i].name;
+                    defparameters[i].Value = lstParameters[i].value;
+                    defparameters[i].SqlDbType = lstParameters[i].type;
+                    defparameters[i].Size = lstParameters[i].size;
+                    defparameters[i].Precision = lstParameters[i].presision;
+                    defparameters[i].Direction = lstParameters[i].direction;
+                    defparameters[i].Scale = lstParameters[i].scale;
+                }
+
+
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ Common/Helpers/clsSqlParameter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Helpers
+{
+    public class clsSqlParameter
+    {
+        public string name { get; set; }
+        public object value { get; set; }
+        public SqlDbType type { get; set; }
+        public ParameterDirection direction { get; set; }
+        public int size { get; set; }
+        public byte presision { get; set; }
+        public byte scale { get; set; }
+
+        /// <summary>
+        /// Construct method for manage the parameters
+        /// </summary>
+        /// <param name="p1">name</param>
+        /// <param name="p2">value</param>
+        /// <param name="p3">type</param>
+        /// <param name="p4">direction</param>
+        /// <param name="p5">size</param>
+        public clsSqlParameter(string p1, object p2, SqlDbType p3, ParameterDirection p4, int p5) {
+            name = p1;
+            value = p2;
+            type = p3;
+            direction = p4;
+            size = p5;
+            presision = 0;
+            scale = 0;
+        }
+
+    }
+}
